ed91e49c882b220dfb84e7e0fa6d30e3716c6720
Ali Hamza <ranaalihamza903@gmail.com>
2026-06-21 00:37:42 -0700

    fix cors and update backend url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://agriprime-n5ao.onrender.com',
+    credentials: true,
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(morgan('dev'));
