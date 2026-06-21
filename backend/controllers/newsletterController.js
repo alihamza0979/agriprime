914f1d88abf7b56ad23f50e7f761a976fddc5853
Ali Hamza <ranaalihamza903@gmail.com>
2026-06-21 16:14:11 -0700

    fix: added missing newsletterController.js

---
+++ backend/controllers/newsletterController.js
@@ -0,0 +1,29 @@
+const { sendEmail } = require('../services/emailService');
+
+exports.subscribe = async (req, res) => {
+    try {
+        const { email } = req.body;
+        
+        if (!email) {
+            return res.status(400).json({ message: 'Email is required' });
+        }
+
+        // Try sending a welcome email
+        try {
+            const html = `
+                <h3>Welcome to the AgriPrime Community!</h3>
+                <p>Hi there,</p>
+                <p>Thank you for subscribing to our newsletter! You'll now receive exclusive offers, farming tips, and early access to new products.</p>
+                <br/><p>Best regards,<br/>AgriPrime Team</p>
+            `;
+            await sendEmail(email, 'Welcome to AgriPrime Newsletter!', html);
+        } catch (emailErr) {
+            console.error('Failed to send newsletter welcome email:', emailErr);
+            // We can still return success even if email fails due to bad config
+        }
+
+        res.status(200).json({ success: true, message: 'Thanks for subscribing!' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
